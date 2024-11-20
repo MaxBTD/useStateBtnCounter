2b326adc5e4d92be87121706b34531f870b29074
5m <konradek982@gmail.com>
2024-11-20 12:48:25 +0100

    increase decrease buttons added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={() => {setCount(count+1)}} id="zwiekszBtn">Zwiększ</button>
+      <button onClick={() => {count>0 && setCount(count-1)}} id="zmniejszBtn">Zmniejsz</button>
     </div>
   );
 }
